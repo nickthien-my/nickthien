4062198ef8bd1b0d7e497e39b3a3786fd193d64d
nickthien-my <84914767+nickthien-my@users.noreply.github.com>
2022-02-09 22:35:41 +0800

    First Update

---
+++ articles/scripts/latest.js
@@ -0,0 +1,39 @@
+latest(articles);
+
+function latest(articles) {
+  var size = 4;
+  latestItems = articles.slice(0, size);
+
+  var div = document.getElementById('rowLatest');
+  latestItems.forEach((item) => {
+    const card = document.createElement('div');
+    card.classList.add('card', 'm-2');
+    card.style.width = "16rem";
+    const coverImg = document.createElement('img');
+    coverImg.classList.add('card-img-top');
+    coverImg.src = item.coverimg;
+    card.appendChild(coverImg);
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = item.title;
+    cardBody.appendChild(cardTitle);
+    const cardSubtitle = document.createElement('h6');
+    cardSubtitle.classList.add('card-subitle', 'mb-2', 'text-muted');
+    cardSubtitle.textContent = item.subtitle;
+    cardBody.appendChild(cardSubtitle);
+    const cardDate = document.createElement('p');
+    cardDate.classList.add('card-text');
+    cardDate.style.fontweight = "300";
+    cardDate.textContent = item.date;
+    cardBody.appendChild(cardDate);
+    const cardLink = document.createElement('a');
+    cardLink.classList.add('card-link');
+    cardLink.href = item.category + "/" + item.url;
+    cardLink.textContent = "Read";
+    cardBody.appendChild(cardLink);
+    card.appendChild(cardBody);
+    div.append(card);
+  })
+}

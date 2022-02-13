eedcefd757e2b1a1c1618ec0ae474d804e84a875
nickthien-my <84914767+nickthien-my@users.noreply.github.com>
2022-02-13 17:33:36 +0800

    Update (13-2-22)
    Quick linking fix.

--- writtenwork/scripts/articles.js
+++ writtenwork/scripts/articles.js
@@ -3,7 +3,7 @@ var articles = [
    "date": "2017-11-02",
    "title": "Bunking In with Team Rough Life",
    "subtitle": "Extending a helping hand to the Malaysian representatives of ieSF LoL World Championship 2017.",
-   "coverimg": "./img/placeholder/placeholder_esports_fdg.jpg",
+   "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
    "category": "esports",
    "url": "bunking-in-with-team-rough-life.html"
  },
@@ -11,7 +11,7 @@ var articles = [
    "date": "2017-08-23",
    "title": "Team Interview | Pre-ESL Playoffs | Overwatch",
    "subtitle": "Part time team, full time heroes. Getting to know the FDG Overwatch team.",
-   "coverimg": "./img/placeholder/placeholder_esports_fdg.jpg",
+   "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
    "category": "esports",
    "url": "team-interview-overwatch.html"
  },
@@ -19,7 +19,7 @@ var articles = [
    "date": "2017-7-18",
    "title": "Weekly Wrap-Up - 18/7/2017",
    "subtitle": "Keeping you up to date with what happened in FDG over the past week.",
-   "coverimg": "./img/placeholder/placeholder_esports_fdg.jpg",
+   "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
    "category": "esports",
    "url": "weeklywrapup1872017.html"
  },
@@ -27,7 +27,7 @@ var articles = [
    "date": "2017-08-16",
    "title": "TLC (MY) Summer Season’s Outstanding Rookie JLKC Interview",
    "subtitle": "Getting to know our 'Outstanding Rookie''s thoughts after his awards.",
-   "coverimg": "./img/placeholder/placeholder_esports_fdg.jpg",
+   "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
    "category": "esports",
    "url": "jlkc-interview.html"
  },
@@ -35,7 +35,7 @@ var articles = [
    "date": "2017-7-12",
    "title": "The Heroes' Call",
    "subtitle": "Taking a look at some of the up and coming events happening around Overwatch.",
-   "coverimg": "./img/placeholder/placeholder_esports_fdg.jpg",
+   "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
    "category": "esports",
    "url": "the-heroes-call.html"
  },
@@ -43,7 +43,7 @@ var articles = [
    "date": "2017-6-20",
    "title": "Weekly Wrap-Up - 20/6/2017",
    "subtitle": "Keeping you up to date with what happened in FDG over the past week.",
-   "coverimg": "./img/placeholder/placeholder_esports_fdg.jpg",
+   "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
    "category": "esports",
    "url": "weekly-wrapup-2062017.html"
  },
@@ -51,7 +51,7 @@ var articles = [
    "date": "2017-10-04",
    "title": "Overwatch Redbull Coliseum Pre-game Interview",
    "subtitle": "Before taking the fight to the Redbull Coliseum, we check in on the Overwatch team.",
-   "coverimg": "./img/placeholder/placeholder_esports_fdg.jpg",
+   "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
    "category": "esports",
    "url": "overwatch-redbull-coliseum-pregame-interview.html"
  }

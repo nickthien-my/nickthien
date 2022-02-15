aa94575132fcdbe2f30da6d75adab365fc7c7c64
nickthien-my <84914767+nickthien-my@users.noreply.github.com>
2022-02-15 23:34:24 +0800

    Update (15-2-22)

---
+++ writtenwork/esports/the-cs-go-report.js
@@ -0,0 +1,62 @@
+const post = {
+    "date": "2017-11-17",
+    "title": "The CSGO Report",
+    "subtitle": "After close to a month of competing overseas, these are some of the experiences that our players have left with.",
+    "coverimg": "https://nickthien.com/writtenwork/img/placeholder/placeholder_esports_fdg.jpg",
+    "category": "esports",
+    "body": [
+      {
+        "type": "text",
+        "output": "After a month long international campaign that includes several big tournaments such as the IeSF World Championship, the ROG Masters APAC Qualifiers and the eXTREMESLAND ZOWIE Asia tournament. In total, they've flown an approximate distance of 21,000 kilometers to represent Malaysia in <em>Counter-Strike: Global Offensive</em>. While the team takes a quick break to reflect on their performance and how to improve, we wanted to get a quick interview in to ask them what has been their most memorable event during the campaign.",
+      },
+      {
+        "type": "header",
+        "output": "Aiman \"aimannn\" Azham",
+      },
+      {
+        "type": "text",
+        "output": "<em>For Shanghai, it was definitely the after-party! It was so well organized and we had a good time!<br>Our CSGO manager, Ng, and I went out to the “Mall of Asia” in Manila, it was so happening! I felt we performed well in the tournament there as well.<br>We went out for street food in Busan, the Barbeque Chicken and Beef was to die for. The weather was nice and cold as well.</em>",
+      },
+      {
+        "type": "header",
+        "output": "Taschvind \"Taz\" Sielan Jayasielan",
+      },
+      {
+        "type": "text",
+        "output": "<em>To me, my unforgettable experience was walking up the stage at the IeSF 9th World Championships 2017 as we represented Malaysia in esports and the opportunity to compete and rub shoulders with the pro players from all around the world.</em>",
+      },
+      {
+        "type": "header",
+        "output": "Idzni \"click-\" Rosdi",
+      },
+      {
+        "type": "text",
+        "output": "<em>I made quite a number of new friends throughout my experience overseas, I will especially cherish the connections I made with the pro players from other countries. The after-party in Shanghai was great as well.</em>",
+      },
+      {
+        "type": "header",
+        "output": "Khalish \"d4v41\" Rusyaidee",
+      },
+      {
+        "type": "text",
+        "output": "<em>I love the food in South Korea, I regret having only found the street food vendors near where we stayed until the last day! I can't get the Korean BBQ out of my mind.</em>",
+      },
+      {
+        "type": "header",
+        "output": "Ridzuan \"3nable\" Hazi",
+      },
+      {
+        "type": "text",
+        "output": "<em>I played my first international tournament in Shanghai against other international teams. I was nervous but it's not an experience that I will ever forget!<br>I had my first Jollibee experience in Manila, it was amazing.</em>",
+      },
+      {
+        "type": "header",
+        "output": "Lee \"y00nghwe1\" Yoong Hwei",
+      },
+      {
+        "type": "text",
+        "output": "<em>I will never forget walking around Shanghai and Busan at night with the team as we hung out, had great food and the amazing weather. It was like 12 degrees and I enjoyed it a lot!</em>",
+      }
+    ]
+  }
+  

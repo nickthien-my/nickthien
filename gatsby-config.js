4325b9bf0a935079a4bbec662b449db7aff042ec
nickthien-my <84914767+nickthien-my@users.noreply.github.com>
2022-11-05 17:21:39 +0800

    Gatsby First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `Nicholas Thien`,
+    description: `This is my portfolio page.`,
+    author: `Nicholas Thien`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassOptions: {
+          precision: 6,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Nicholas Thien`,
+        short_name: `Nick Thien`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#0066ff`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+  ],
+}
